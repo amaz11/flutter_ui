541aae59772602982e640efc2130f8576615c9d4
amaz11 <k.m.shaonred@gmail.com>
2023-12-13 13:44:53 +0600

    "adding provider"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/pages/Authentication_service.dart';
+import 'package:provider/provider.dart';
 // import 'package:flutter_ui/pages/APage.dart';
 // import 'package:flutter_ui/pages/NewPage.dart';
 // import 'package:flutter_ui/pages/TwitterForm.dart';
@@ -15,96 +18,102 @@ class MyAPP extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // If routes are declared then home key is no longer required.
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance)),
+      ],
+      child: MaterialApp(
+        // If routes are declared then home key is no longer required.
 
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //       backgroundColor: Colors.green[400],
-      //       title: const Center(
-      //         child: Text(
-      //           'Home',
-      //           style: TextStyle(
-      //             fontSize: 18.5,
-      //             color: Colors.white,
-      //           ),
-      //         ),
-      //       )),
-      //   body: Column(
-      //       // padding: const EdgeInsets.all(10),
-      //       children: [
-      //         Image.network(
-      //           'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-      //           height: 150,
-      //           width: 150,
-      //         ),
-      //         const SizedBox(height: 15),
-      //         Container(
-      //           padding: const EdgeInsets.only(left: 10, right: 10),
-      //           child: const TextField(
-      //             obscureText: false,
-      //             decoration: InputDecoration(
-      //               border: OutlineInputBorder(),
-      //               labelText: 'User Name',
-      //             ),
-      //           ),
-      //         ),
-      //         const SizedBox(height: 15),
-      //         const Row(
-      //           mainAxisAlignment: MainAxisAlignment.center,
-      //           children: [
-      //             Text('Norification'),
-      //             SwitchNot(),
-      //           ],
-      //         ),
-      //         const SizedBox(height: 15),
-      //         Container(
-      //           decoration: BoxDecoration(
-      //             borderRadius: BorderRadius.circular(5),
-      //             color: Colors.green,
-      //           ),
-      //           child: TextButton(
-      //             onPressed: () {},
-      //             style: TextButton.styleFrom(
-      //               foregroundColor: Colors.white,
-      //               // backgroundColor: Colors.green,
-      //               padding: const EdgeInsets.all(16),
-      //               // minimumSize: const Size.fromWidth(20)
-      //               textStyle: const TextStyle(fontSize: 20),
-      //             ),
-      //             child: const Text('Save'),
-      //           ),
-      //         ),
-      //         ElevatedButton(
-      //           onPressed: () {},
-      //           child: const Text('Save 2'),
-      //         )
-      //       ]),
-      // ),
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //       backgroundColor: Colors.green[400],
+        //       title: const Center(
+        //         child: Text(
+        //           'Home',
+        //           style: TextStyle(
+        //             fontSize: 18.5,
+        //             color: Colors.white,
+        //           ),
+        //         ),
+        //       )),
+        //   body: Column(
+        //       // padding: const EdgeInsets.all(10),
+        //       children: [
+        //         Image.network(
+        //           'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+        //           height: 150,
+        //           width: 150,
+        //         ),
+        //         const SizedBox(height: 15),
+        //         Container(
+        //           padding: const EdgeInsets.only(left: 10, right: 10),
+        //           child: const TextField(
+        //             obscureText: false,
+        //             decoration: InputDecoration(
+        //               border: OutlineInputBorder(),
+        //               labelText: 'User Name',
+        //             ),
+        //           ),
+        //         ),
+        //         const SizedBox(height: 15),
+        //         const Row(
+        //           mainAxisAlignment: MainAxisAlignment.center,
+        //           children: [
+        //             Text('Norification'),
+        //             SwitchNot(),
+        //           ],
+        //         ),
+        //         const SizedBox(height: 15),
+        //         Container(
+        //           decoration: BoxDecoration(
+        //             borderRadius: BorderRadius.circular(5),
+        //             color: Colors.green,
+        //           ),
+        //           child: TextButton(
+        //             onPressed: () {},
+        //             style: TextButton.styleFrom(
+        //               foregroundColor: Colors.white,
+        //               // backgroundColor: Colors.green,
+        //               padding: const EdgeInsets.all(16),
+        //               // minimumSize: const Size.fromWidth(20)
+        //               textStyle: const TextStyle(fontSize: 20),
+        //             ),
+        //             child: const Text('Save'),
+        //           ),
+        //         ),
+        //         ElevatedButton(
+        //           onPressed: () {},
+        //           child: const Text('Save 2'),
+        //         )
+        //       ]),
+        // ),
 
-      // routes: {
-      //   '/': (context)=> Page Class Name
-      // },
-      // routes: {
-      //   '/': (context) => const MyHome(),
-      //   '/apage': (context) => const APage(),
-      //   '/newPage': (context) => const NewPage(),
-      // },
-      // global theme settings
-      home: const AuthenticatWapper(),
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            color: Colors.blueAccent,
-            toolbarHeight: 50,
-            shadowColor: Colors.red,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50)))),
+        // routes: {
+        //   '/': (context)=> Page Class Name
+        // },
+        // routes: {
+        //   '/': (context) => const MyHome(),
+        //   '/apage': (context) => const APage(),
+        //   '/newPage': (context) => const NewPage(),
+        // },
+        // global theme settings
+        home: const AuthenticatWapper(),
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              color: Colors.blueAccent,
+              toolbarHeight: 50,
+              shadowColor: Colors.red,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50)))),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
